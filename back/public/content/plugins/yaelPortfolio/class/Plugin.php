ce66abbebe117c5d125f7615d9bc2925ff552201
yaelx <yaelx@localhost>
2021-08-25 14:07:05 +0200

    create plugin YaelPortfolio in wordpress

---
+++ back/public/content/plugins/yaelPortfolio/class/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace YaelPortfolio;
+
+class Plugin 
+{
+    public function activate()
+    {
+        // $this->registerRole();
+    }
+
+    public function deactivate()
+    {
+        // remove_role('user');
+    }
+}
---
+++ back/public/content/plugins/yaelPortfolio/vendor-yaelPortfolio/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9648c9bfecb14feb6dc8f9772c0271052b54caaf',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9648c9bfecb14feb6dc8f9772c0271052b54caaf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
